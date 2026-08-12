b3c0c07581b8085f279d96a089ba7a197af2919d
Shoaib Ejaz <308232@ust.com>
2026-08-12 15:22:24 +0530

    Day 1 Task

--- day1.py
+++ day1.py
@@ -153,5 +153,4 @@ score3 = float(input("Enter third test score: "))
 
 average = (score1 + score2 + score3) / 3
 has_passed = (average >= 40)
-
 print(f"Average: {average:.1f} | Passed: {has_passed}")
