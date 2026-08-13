c8ff7795c67468814c8ad5dd2a16e46d685e1363
Lalnarendra <159632326+Lalnarendra@users.noreply.github.com>
2026-08-13 13:03:45 +0530

    Delete currency.py

--- currency.py
+++
@@ -1,4 +0,0 @@
-dollar = float(input('enter the dollar: )'))
-conv_rate =  float(input('enter the conversion rate: '))
-rupee = dollar * conv_rate
-print(f'{dollar:.2f} Dollar = {rupee:.2f} Rupees')
