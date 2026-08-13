0105237d92476e6a8e24964917d3fe93e2fd9947
joeljobin8-cloud <joeljobin8@gmail.com>
2026-08-13 09:41:31 +0530

    Update hello_python.py

--- src/day1/hello_python.py
+++ src/day1/hello_python.py
@@ -1,1 +1,16 @@
+name = "Joel"
+age = 24
+height = 1.67
+coded_before = True
 
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(coded_before))
+
+fav_string = input("Enter your favourite number : ")
+
+fav_num = int(fav_string) * 2
+print(fav_num)
+
+print(f"{name} is {age} years old and {height} meters tall.")
