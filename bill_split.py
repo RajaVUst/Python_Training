cea4796be041b464f0d2a52e1dcf3a66ebdb1745
Lalnarendra <159632326+Lalnarendra@users.noreply.github.com>
2026-08-13 13:02:53 +0530

    Delete bill_split.py

--- bill_split.py
+++
@@ -1,9 +0,0 @@
-bill = float(input('Enter the total bill amount: '))
-people = int(input('No of people: '))
-tip = input('do you want to add tip? (yes/no): ')
-wants_tip =  tip == 'yes'
-if wants_tip:
-    bill = bill * 1.10
-share = bill / people    
-print(f'Each person pays: {share:.2f}')
-
