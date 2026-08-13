224db3b58b71f394334c8c7857202b8c9151c07b
vinayteja378-art <vinayteja378@gmail.com>
2026-08-13 15:27:50 +0530

    Delete llm-chatbot/chatbot.py

--- llm-chatbot/chatbot.py
+++
@@ -1,4 +0,0 @@
-name=input("Enter your name: ")
-age=input("Enter your age: ")
-place=input("Enter your place: ")
-print("Hello " + name + "you are " + age + " years old and you live in " + place + ".")
