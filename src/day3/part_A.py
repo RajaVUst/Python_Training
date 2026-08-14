e7459cbb4ecc2ce19ab03d72f5e4d934103a08b8
joeljobin8-cloud <joeljobin8@gmail.com>
2026-08-14 14:19:51 +0530

    Create part_A.py
    Part A completed

---
+++ src/day3/part_A.py
@@ -0,0 +1,20 @@
+#Part A
+a = 98
+b = 77.7
+c = "Hello"
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(a/b)
+
+string = "Python Readiness Training"
+print(string[0:6])
+print(string[::-1])
+
+print(a == b) 
+print(a != b) 
+print(a > b) 
+print(a <= b)
+
