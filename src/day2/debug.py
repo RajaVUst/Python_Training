2f3a8aba07b1fd87970af2ef148517b2dbb70054
joeljobin8-cloud <joeljobin8@gmail.com>
2026-08-17 10:04:28 +0530

    Debug excercises

---
+++ src/day2/debug.py
@@ -0,0 +1,19 @@
+#Debug 
+print("Start")
+print("Middle")
+print("End")
+
+age = 25
+message = "I am " + str(age) + " years old"
+print(message)
+
+
+username = "Joel Johnson
+print(f"Welcome, {username}!")
+
+word = "Python"
+print(word[5])
+
+sentence = "one two three four"
+word_list = sentence.split()
+print(f"Word count: {len(word_list)}")
